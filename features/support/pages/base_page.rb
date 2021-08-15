373d2547d000051f49db93e1f854a416df6f8f7a
gsporto98 <gsporto98@hotmail.com>
2021-08-15 15:20:11 -0300

    Mapeamento header OK

---
+++ features/support/pages/base_page.rb
@@ -0,0 +1,142 @@
+require_relative "../env.rb"
+
+class HomePage
+
+    attr_accessor :driver
+    def initialize(webDriver)
+        @driver = webDriver
+    end
+
+    # Mapeamento
+
+        def text_homeDesc()
+            self.driver.find_element(:css, ".HomeHero-module--description--2Ua8u")
+        end
+    
+        def btnIcon_riot()
+            self.driver.find_element(:css, "#riotbar-left-content > div._2kifR8SgDiOjjbRMOZ1npZ.riotbar-branding-switcher > a > span")
+        end
+
+        def btnIcon_valorant()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2TEq--nzhVKfWiAYjLHjyN.riotbar-desktop-logo-outer > div > a")
+        end
+
+        def combobox_InformacoesDoJogo()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(1) > a")
+        end
+
+        def select_agentes()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(1) > a > ul > li:nth-child(1) > a")
+        end
+
+        def select_mapas()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(1) > a > ul > li:nth-child(2) > a")
+        end
+ 
+        def select_arsenal()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(1) > a > ul > li:nth-child(3) > a")
+        end
+ 
+        def btn_midia()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(2) > a")
+        end
+
+        def btn_noticias()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(3) > a")
+        end
+
+        def btn_placar()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(4) > a")
+        end
+
+        def combobox_suporte()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(5) > a")
+        end
+
+        def select_requisitos()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(5) > a > ul > li:nth-child(1) > a")
+        end
+
+        def select_suporte()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(5) > a > ul > li:nth-child(2) > a")
+        end
+ 
+        def select_CodigoDaComunidade()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(5) > a > ul > li:nth-child(3) > a")
+        end
+
+        def comboboxSocial()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(6) > a")
+        end
+
+        def select_facebook()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(6) > a > ul > li:nth-child(1) > a")
+        end
+
+        def select_youtube()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(6) > a > ul > li:nth-child(2) > a")
+        end
+
+        def select_instagram()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(6) > a > ul > li:nth-child(3) > a")
+        end
+
+        def select_twitter()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(6) > a > ul > li:nth-child(4) > a")
+        end
+
+        
+        def btn_esports()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(7) > a")
+        end
+
+        
+        def combobox_comunidade()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(8) > a")
+        end
+
+        def select_discord()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(8) > a > ul > li:nth-child(1) > a")
+        end
+
+        def select_forum()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(8) > a > ul > li:nth-child(2) > a")
+        end
+
+        def select_grupoFacebook()
+            self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(8) > a > ul > li:nth-child(3) > a")
+        end
+
+        def select_torneioComunidade()
+                self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(8) > a > ul > li:nth-child(4) > a")
+        end
+ 
+        def select_ProgramaDeApoio()
+                self.driver.find_element(:css, "#riotbar-center-content > div._2Jx8cIlm63mjPBNcz3mjwO.riotbar-desktop-navigation-wrapper > div:nth-child(8) > a > ul > li:nth-child(5) > a")
+        end
+
+        def btnIcon_idioma()
+            self.driver.find_element(:css, "#riotbar-right-content > div._1K9T69nrXajaz_b4HNuhtI.riotbar-locale-switcher > div > a")
+        end
+
+        def btn_jogueAgora()
+            self.driver.find_element(:css, "#riotbar-right-content > div.undefined.riotbar-account-reset._2f9sdDMZUGg63xLkFmv-9O.riotbar-account-container > div > a")
+        end
+ 
+        def btn_jogueGratis()
+            self.driver.find_element(:css, "#gatsby-focus-wrapper > section.section.dark.HomeHero-module--homeHero--1drYm.in-view > div.sectionWrapper.border-left.HomeHero-module--sectionWrapper--3hTRk > button > div > span.PrimaryButton-module--label-text--23ce5")
+        end
+
+
+        
+    # end
+
+    # Métodos
+        def irParaPagina()
+            @driver.navigate.to "https://playvalorant.com/pt-br/"
+
+        end
+
+    # end
+
+end
